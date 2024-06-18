3065352b49eaae4c21cab137ad3d150a418315a2
Dario Vincenzo De Simone <dariovincenzo.desimone@gmail.com>
2024-06-18 18:46:33 +0200

    added ABIs

---
+++ abi/index.ts
@@ -0,0 +1,47 @@
+import contractAbiJson from "../abi/abi.json";
+import contractAbiV4Json from "../abi/abiV4.json";
+import contractAbiDiamond from "../abi/abiDiamond.json";
+import contractAbiETHJson from "../abi/abi-eth.json";
+import tokenAbiJson from "../abi/abi-token.json";
+import merkleClaimAbi from "../abi/abi-merkle-claim.json";
+import merkleAirdrop from "../abi/MerkleDistributor.json";
+import lotteryAbiJson from "../abi/Lottery.json";
+import merkleMBSaleAbi from "../abi/MBSaleV2.json"
+import stakingRewardsAbiJson from "../abi/StakingRewards.json";
+import pancakeV2Pair from "../abi/PancakeV2Pair.json";
+import pancakeV2Abi from "../abi/pancakeABI.json";
+import redeemNFTABI from "../abi/RedeemNFT.json";
+import BNBAbi from "../abi/BNBAbi.json";
+import ERC721 from "../abi/ERC721.json";
+import marketplaceAbi from '../abi/marketplaceABI.json';
+import lootboxAbi from '../abi/lootboxABI.json';
+import playdexAbi from '../abi/playdexABI.json';
+import ERC721BID from "../abi/erc721bid.json"
+import CCIPSender from "../abi/ccipSenderAbi.json"
+import BabyDogeAbi from "../abi/babyDogeAbi.json"
+import CryptoDotComNFTAbi from "../abi/cryptodotComNftAbi.json"
+
+export {
+    contractAbiJson,
+    contractAbiV4Json,
+    contractAbiDiamond,
+    contractAbiETHJson,
+    tokenAbiJson,
+    merkleClaimAbi,
+    merkleAirdrop,
+    lotteryAbiJson,
+    merkleMBSaleAbi,
+    stakingRewardsAbiJson,
+    pancakeV2Pair,
+    pancakeV2Abi,
+    redeemNFTABI,
+    BNBAbi,
+    ERC721,
+    marketplaceAbi,
+    lootboxAbi,
+    playdexAbi,
+    ERC721BID,
+    CCIPSender,
+    BabyDogeAbi,
+    CryptoDotComNFTAbi
+}
